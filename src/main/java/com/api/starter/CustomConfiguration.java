a20f8fca6eede0b1737d5be656464f1d5ee150fe
sehwan-choi <nu940907@gmail.com>
2024-03-04 18:45:19 +0900

    AutoConfiguration 추가

---
+++ src/main/java/com/api/starter/CustomConfiguration.java
@@ -0,0 +1,71 @@
+package com.api.starter;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import feign.FeignException;
+import feign.Response;
+import feign.codec.Decoder;
+import feign.codec.ErrorDecoder;
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import org.springframework.cloud.openfeign.support.ResponseEntityDecoder;
+import org.springframework.cloud.openfeign.support.SpringDecoder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+
+import java.io.IOException;
+import java.util.Collections;
+
+@Configuration
+public class CustomConfiguration {
+
+    @Bean
+    public ErrorDecoder userErrorDecoder() {
+        return new ErrorDecoder() {
+            @Override
+            public Exception decode(String methodKey, Response response) {
+                System.out.println("userErrorDecoder!!!!!!!!!!!!!!!");
+                HttpStatus status = HttpStatus.valueOf(response.status());
+                try {
+                    if (status.is4xxClientError()) {
+                        return new FeignException.FeignClientException(status.value(), response.reason(), response.request(), response.body().asInputStream().readAllBytes(), response.headers());
+                    } else {
+                        return new FeignException.FeignServerException(status.value(), response.reason(), response.request(), response.body().asInputStream().readAllBytes(), response.headers());
+                    }
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        };
+    }
+
+    @Bean
+    public Decoder photoDecoder() {
+        ObjectMapper objectMapper = new ObjectMapper()
+                .registerModule(new JavaTimeModule())
+                .setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE)
+                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+        MappingJackson2HttpMessageConverter jackson2HttpMessageConverter = new MappingJackson2HttpMessageConverter(objectMapper);
+        ObjectFactory<HttpMessageConverters> objectFactory = () -> new HttpMessageConverters(Collections.singletonList(jackson2HttpMessageConverter));
+
+        return new ResponseEntityDecoder(new SpringDecoder(objectFactory));
+    }
+
+    @Bean
+    public Decoder userDecoder() {
+        ObjectMapper objectMapper = new ObjectMapper()
+                .registerModule(new JavaTimeModule())
+                .setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE)
+                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+        MappingJackson2HttpMessageConverter jackson2HttpMessageConverter = new MappingJackson2HttpMessageConverter(objectMapper);
+        ObjectFactory<HttpMessageConverters> objectFactory = () -> new HttpMessageConverters(Collections.singletonList(jackson2HttpMessageConverter));
+
+        return new ResponseEntityDecoder(new SpringDecoder(objectFactory));
+    }
+}
--- src/main/java/com/api/starter/StarterApplication.java
+++ src/main/java/com/api/starter/StarterApplication.java
@@ -1,20 +1,120 @@
 package com.api.starter;
 
-import com.mylib.api.MyLibConfig;
+import com.mylib.photo.EnablePhotoApiClients;
+import com.mylib.photo.client.PhotoApiClient;
+import com.mylib.photo.client.dto.PhotoResponse;
+import com.mylib.post.EnablePostApiClients;
+import com.mylib.post.client.dto.PostResponse;
+import com.mylib.post.service.PostApiService;
+import com.mylib.user.EnableUserApiClients;
+import com.mylib.user.client.dto.UserResponse;
+import com.mylib.user.service.UserApiService;
+import feign.FeignException;
 import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.List;
+
 @SpringBootApplication
+@RequiredArgsConstructor
+@EnablePhotoApiClients
+@EnableUserApiClients
+@EnablePostApiClients
 public class StarterApplication {
 
 	@Autowired
-	MyLibConfig.ApiUtil apiUtil;
+	UserApiService userApiService;
+
+	@Autowired
+	PostApiService postApiService;
+
+	@Autowired
+	PhotoApiClient photoApiService;
 
 	@PostConstruct
-	public void init() {
-		System.out.println("apiUtil = " + apiUtil);
+	public void init () throws NoSuchAlgorithmException {
+		user();
+		post();
+		photo();
+	}
+
+	private void user() throws NoSuchAlgorithmException {
+		System.out.println("==========user==========");
+		System.out.println("service = " + userApiService);
+		List<UserResponse> users = userApiService.getUsers();
+		for (UserResponse user : users) {
+			System.out.println("user = " + user);
+		}
+
+		System.out.println("====================");
+		int randomUserId = SecureRandom.getInstanceStrong().nextInt(1, 10);
+		UserResponse user = userApiService.getUser(String.valueOf(randomUserId));
+		System.out.println("user = " + user);
+
+		try {
+			userApiService.getUser("999");
+		} catch (FeignException.FeignClientException e) {
+			System.out.println("e = " + e);
+		} catch (FeignException.FeignServerException e1) {
+			System.out.println("e1 = " + e1);
+		} catch (FeignException e2) {
+			System.out.println("e2 = " + e2);
+		}
+	}
+
+	private void post() throws NoSuchAlgorithmException {
+		System.out.println("==========post==========");
+		System.out.println("postApiService = " + postApiService);
+		List<PostResponse> posts = postApiService.getPosts();
+		for (PostResponse post : posts) {
+			System.out.println("post = " + post);
+		}
+
+		System.out.println("====================");
+		int randomUserId = SecureRandom.getInstanceStrong().nextInt(1, 10);
+		PostResponse post = postApiService.getPost(String.valueOf(randomUserId));
+		System.out.println("post = " + post);
+
+		try {
+			postApiService.getPost("999");
+		} catch (FeignException.FeignClientException e) {
+			System.out.println("e = " + e);
+		} catch (FeignException.FeignServerException e1) {
+			System.out.println("e1 = " + e1);
+		} catch (FeignException e2) {
+			System.out.println("e2 = " + e2);
+		}
+
+	}
+
+	private void photo() throws NoSuchAlgorithmException {
+		System.out.println("==========photo==========");
+		System.out.println("photoApiService = " + photoApiService);
+		List<PhotoResponse> photos = photoApiService.getPhotos();
+		for (PhotoResponse photo : photos) {
+			System.out.println("photo = " + photo);
+		}
+
+		System.out.println("====================");
+		int randomUserId = SecureRandom.getInstanceStrong().nextInt(1, 10);
+		PhotoResponse photo = photoApiService.getPhoto(String.valueOf(randomUserId));
+		System.out.println("photo = " + photo);
+
+		try {
+			photoApiService.getPhoto("99999");
+		} catch (FeignException.FeignClientException e) {
+			System.out.println("e = " + e);
+		} catch (FeignException.FeignServerException e1) {
+			System.out.println("e1 = " + e1);
+		} catch (FeignException e2) {
+			System.out.println("e2 = " + e2);
+		}
+
 	}
 
 	public static void main(String[] args) {
